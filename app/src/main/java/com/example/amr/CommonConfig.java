0a225c89c46dddcfa9bdff3eab75ef3abad56326
李光浩 <wsmslgh@gmail.com>
2021-10-02 09:15:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/amr/CommonConfig.java
@@ -0,0 +1,7 @@
+package com.example.amr;
+
+public class CommonConfig {
+    public static String SERVER_IP_ADDRESS="192.168.6.103";
+    public static int AUDIO_SERVER_UP_PORT=5566;
+    public static int AUDIO_SERVER_DOWN_PORT=5566;
+}
